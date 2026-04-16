618f866a9ef22965f3b8b1dde26b8f0b384e06ee
Romeu-ce <newagent332@gmail.com>
2026-04-16 23:51:42 +0300

    Update SensorsControl.cpp
    changes to data processing. Higher input voltage allowance

--- SensorsControl.cpp
+++ SensorsControl.cpp
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 #include <DHT.h>
 
-DHT dht(22,DHT11) //22 - is the pin used
+DHT dht(27,DHT11) //22 - is the pin used
 
 struct Result {
     float temp;
@@ -17,12 +17,13 @@ Result control2(){
   float temp =dht.readTemperature(); // in celc
   float humidity =dht.readHumidity(); // in percent
 
-  float X=(float)analogRead(32);
-  float X1 = X- 112;    //datata error correction
+  float X=(float)analogRead(34); //current readings
+  //float X1 = X- 112;    //datata error correction
   float Vin = ((float)analogRead(36)/4095)*3.29; //pin VP voltage readings
-  float Ain = (X1/4095)*3.29;
-  float Vr = Vin * 4.85;   //real velues
-  float Ar= (Ain-1.1)/ 0.1; ////real velues
+  float Ai = (X/4095)*3.29;
+  float Ain =Ai/2.210526;
+  float Vr = Vin * 11;   //real velues ( 4.85)
+  float Ar= (Ain-1.1)/ 0.1; //real velues
   voltage = Vr;
   current = Ar;
 
