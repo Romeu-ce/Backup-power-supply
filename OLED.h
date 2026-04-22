6b1a770b6c0876f678b10093d8e43a2ea3cc5c4d
Romeu-ce <newagent332@gmail.com>
2026-04-22 22:35:51 +0300

    Update
    bug fixes

--- OLED.h
+++ OLED.h
@@ -1,8 +1,9 @@
 #ifndef OLED
 #define OLED
 
+extern bool screen_active;
+extern unsigned long screen_start;
+extern const unsigned long SCREEN_DURATION; // 10 seconds
 
-void screen(float a,float b);
-
-#endif
+void screen(float a,float b,float c);
 #endif
